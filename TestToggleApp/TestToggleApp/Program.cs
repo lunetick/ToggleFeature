77434ee9aa00cccb096b656595fd733ac1b040e2
eric.cossette <eric.cossette@gmail.com>
2016-06-08 23:13:05 -0400

    Add comments

--- TestToggleApp/TestToggleApp/Program.cs
+++ TestToggleApp/TestToggleApp/Program.cs
@@ -11,6 +11,13 @@ namespace TestToggleApp
         static void Main(string[] args)
         {
             Console.WriteLine("We test toggle dll here! Ant it will work nice with jenkins");
+
+            // TODO dyn load a C .dll 
+
+            // TODO dyn load a c++ mfc dll
+
+            // TODO dyn load a COM dll (interface to the 2 before ?)
+            
             Console.Write("\nPress any key to continue... ");
             Console.ReadLine();
         }
